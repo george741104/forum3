b50152df7447f86798a9826633e01f78dc6eff90
George Chang <georgec1104@gmail.com>
2015-06-22 17:54:39 +0800

    post CRUD completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,52 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  def index
+
+    @posts = Post.all
+
+    @posts = @posts.page(params[:page]).per(10)
+
+  end
+
+  def show
+    #@post = Post.find(params[:id])
+  end
+
+  def new
+    @post=Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+
+    redirect_to posts_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post.destroy
+
+    flash[:alert] = "Post was deleted"
+    redirect_to :back
+  end
+
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
