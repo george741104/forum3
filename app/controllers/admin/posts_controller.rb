be7eb504f37c00d6d872647beb2c2024199006b2
George Chang <georgec1104@gmail.com>
2015-06-30 22:18:16 +0800

    added admin users and posts page

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,14 +1,20 @@
 class Admin::PostsController < ApplicationController
 
   before_action :authenticate_user!
-
   before_action :check_admin
 
 
-  layout "admin"
+  # layout "admin"
 
   def index
     @posts = Post.all
+    @categories = Category.all
+    @posts = @posts.page(params[:page]).per(10)
+
+    if params[:category_ids]
+      @posts = @posts.joins(:category_postships).where( "category_postships.category_id" => params[:category_ids] )
+    end
+
   end
 
 
