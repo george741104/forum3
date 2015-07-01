f0a9a4999623bd947852c480d8c257b41112920c
George Chang <georgec1104@gmail.com>
2015-07-01 11:23:31 +0800

    complete user role assign controller and update function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,13 +2,18 @@ class Admin::UsersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :check_admin
+  before_action :set_user , only:[:update, :destroy]
 
   def index
     @users = User.all
     @users = @users.page(params[:page]).per(10)
   end
 
+  def update
+    @user.update (user_role_params)
 
+    redirect_to admin_users_path
+  end
 
   def destroy
     @user = User.find(params[:id])
@@ -23,5 +28,16 @@ class Admin::UsersController < ApplicationController
     unless current_user.admin?
       raise ActiveRecord::RecordNotFound
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_role_params
+    params.require(:user).permit(:role)
+
   end
 end
